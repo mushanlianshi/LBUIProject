6833639c68baaf4b6330e235fb30f8fb7db87754
liu bin <liu_bin_home@163.com>
2025-06-09 08:43:13 +0800

    feat：save

--- LBUIProject/Test/LBTestGitChangeFileViewController.m
+++ LBUIProject/Test/LBTestGitChangeFileViewController.m
@@ -18,14 +18,9 @@
     // Do any additional setup after loading the view.
 }
 
-/*
-#pragma mark - Navigation
-
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
+// 测试修改内容，和另外个分支修改文件名比对，会不会对比
+- (void)test_changeFile{
+    
 }
-*/
 
 @end
